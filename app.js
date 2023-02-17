993d21c2de8352a15a794cd8e2bf4bb1bf1a11d1
Arnab <arnabmukherjee58260@gmail.com>
2023-02-17 14:02:17 +0530

    Portfolio website uploaded on github

---
+++ app.js
@@ -0,0 +1,63 @@
+const hamburger = document.querySelector(".hamburger");
+var navlinks = document.querySelector('.nav-links');
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active-nav");
+  navlinks.classList.toggle("active-nav");
+});
+const text = document.querySelector(".second-text");
+const textLoad = ()=>{
+    setTimeout(()=>{
+        text.textContent = "Student";
+    },0);
+    setTimeout(()=>{
+        text.textContent = "Web Developer";
+    },4000);
+}
+textLoad();
+setInterval(textLoad,8000);
+
+var slides = document.querySelectorAll('.slide');
+var btns = document.querySelectorAll('.dot-btn');
+let currentslide = 1;
+var manualnav = function(manual){
+    slides.forEach((slide)=>{
+        slide.classList.remove('active');
+        btns.forEach((btn)=>{
+            btn.classList.remove('active');
+        })
+    })
+    slides[manual].classList.add('active');
+    btns[manual].classList.add('active');
+}
+    btns.forEach((btn, i)=>{
+        btn.addEventListener("click",()=>{
+            manualnav(i);
+            currentslide = i;   2
+        });
+    });
+
+    var repeat = function(activeClass){
+        let active = document.getElementsByClassName('active');
+        let i = 1;
+        var repeater = ()=>{
+            setTimeout(function(){
+                [...active].forEach((activeSlide)=>{
+                    activeSlide.classList.remove('active');
+                })
+                slides[i].classList.add('active');
+                btns[i].classList.add('active');
+                i++;
+                if(slides.length == i){
+                    i = 0;
+                }
+                if(i >= slides.length){
+                    return;
+                }
+                repeater();
+            },5000)
+        }
+        repeater();
+    }
+
+    // repeat();
+
